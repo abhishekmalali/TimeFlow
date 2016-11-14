a4de7f3882ba5b0550232a36cb0312c37f9d8dc4
Abhishek Malali <abhishekmalali@gmail.com>
2016-11-13 19:32:12 -0500

    Adding input batch placeholder

--- timeflow/placeholders/prediction.py
+++ timeflow/placeholders/prediction.py
@@ -23,6 +23,29 @@ def input_placeholder(input_dim, name='inputs'):
     return input
 
 
+def input_batch_placeholder(input_dim, batch_size, name='inputs'):
+    """Initialize input placeholder for prediction networks for batch training
+
+    Parameters
+    ----------
+    input_dim : integer
+        Input dimensions
+    batch_size : integer
+        Input dimensions
+    name : string
+        Placeholder name
+
+    Returns
+    ----------
+    tf.placeholder
+        Input placeholder
+
+    """
+    input = tf.placeholder(tf.float32, shape=[batch_size, None, input_dim],
+                           name=name)
+    return input
+
+
 def output_placeholder(output_dim, name='outputs'):
     """Initialize output placeholder for prediction networks
 
