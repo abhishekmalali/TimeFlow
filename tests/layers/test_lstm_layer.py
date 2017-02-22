84006d7b1289a65d46d0494fd81f75c8f8a19c96
Abhishek Malali <abhishekmalali@gmail.com>
2017-02-21 21:33:40 -0500

    updating the new tests for lstm layers

--- tests/layers/test_lstm_layer.py
+++ tests/layers/test_lstm_layer.py
@@ -12,14 +12,18 @@ def lstm_layer_test():
     in_layer_batch = tflow.layers.InputLSTMLayer(batch_test_placeholder, input_dim, batch_input=True)
     lstm_layer = tflow.layers.LSTMLayer(input_dim, hidden_layer_size, in_layer)
     lstm_layer_batch = tflow.layers.LSTMLayer(input_dim, hidden_layer_size, in_layer_batch)
-    return lstm_layer.get_outputs(), lstm_layer_batch.get_outputs(), hidden_layer_size
+    return lstm_layer.get_outputs(), lstm_layer_batch.get_outputs(), hidden_layer_size, batch_size
 
 
 def test_lstm_layer():
-    lstm_layer_out, lstm_layer_batch_out, hsize = lstm_layer_test()
+    lstm_layer_out, lstm_layer_batch_out, hsize, bsize = lstm_layer_test()
     shape = lstm_layer_out.get_shape()
     assert len(lstm_layer_out.get_shape()) == 3
+    # Checking if hidden size is accurate for non batched inputs
     assert int(shape[2]) == hsize
     shape = lstm_layer_batch_out.get_shape()
-    assert len(lstm_layer_batch_out.get_shape()) == 3
-    assert int(shape[2]) == hsize
+    assert len(lstm_layer_batch_out.get_shape()) == 4
+    # Checking if hidden size is accurate if input and output are batched
+    assert int(shape[3]) == hsize
+    # Checking if batch size is accurate
+    assert int(shape[0]) == bsize
