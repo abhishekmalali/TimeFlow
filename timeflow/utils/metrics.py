435d34e8b14f3cb7de56a9c5b2ce2849bed44465
Abhishek Malali <abhishekmalali@gmail.com>
2017-02-21 21:32:55 -0500

    modifying metrics to be functional in tensorflow 1.0

--- timeflow/utils/metrics.py
+++ timeflow/utils/metrics.py
@@ -17,7 +17,7 @@ def RMSE(predicted_tensor, response_tensor):
     tf.Tensor
         RMSE operation tensor
     """
-    return tf.reduce_mean(tf.pow(tf.sub(predicted_tensor, response_tensor), 2))
+    return tf.reduce_mean(tf.pow(tf.subtract(predicted_tensor, response_tensor), 2))
 
 
 def R2(predicted_tensor, response_tensor):
@@ -38,7 +38,7 @@ def R2(predicted_tensor, response_tensor):
     """
     pred_mean = tf.expand_dims(tf.reduce_mean(predicted_tensor,
                                reduction_indices=[0]), 1)
-    rse = tf.reduce_mean(tf.pow(tf.sub(predicted_tensor, pred_mean), 2))
-    mse = tf.reduce_mean(tf.pow(tf.sub(predicted_tensor, response_tensor), 2))
+    rse = tf.reduce_mean(tf.pow(tf.subtract(predicted_tensor, pred_mean), 2))
+    mse = tf.reduce_mean(tf.pow(tf.subtract(predicted_tensor, response_tensor), 2))
     R2 = 1 - tf.div(mse, rse)
     return R2
